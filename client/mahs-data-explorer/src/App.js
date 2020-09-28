80fd44067241a25300d9df7b92231de9abe69a04
John Sasser <cw.john.sasser@gmail.com>
2020-09-28 13:11:09 -0400

    i need to hop off master branch

---
+++ client/mahs-data-explorer/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import HomePage from './View/HomePage';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
