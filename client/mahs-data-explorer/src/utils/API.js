c9c1a9b113c56a270946eb8f9df20f17cc406d27
John Sasser <cw.john.sasser@gmail.com>
2020-09-28 15:36:43 -0400

    template api functions

--- client/mahs-data-explorer/src/utils/API.js
+++ client/mahs-data-explorer/src/utils/API.js
@@ -1,9 +1,25 @@
 import axios from 'axios';
 
 const APIs = () => {
-  getData: async (url) => {
+  const getData = async (url) => {
     const { data } = await axios.get(url);
     console.log('req successful || data: ', data);
+  };
+
+  const aggregateBySubarea = (geo, indicator) => {
+    console.log(geo, indicator);
+  };
+
+  const exportCSV = (data, params) => {
+    console.log(data, params);
+  };
+
+  const exportPDF = (params) => {
+    console.log(params);
+  };
+
+  const ExportImage = (divID) => {
+    console.log(divID);
   };
 };
 
