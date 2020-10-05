669ac59e4cc8ca8250cdd080b3ca7fbc94ce213d
aagrosse <aagrock81@yahoo.com>
2020-10-05 03:33:46 -0400

    setting up seed function

--- seedDB.js
+++ seedDB.js
@@ -9,50 +9,117 @@ const cityCrossWalkSeed = require('./data/tractToCityCrosswalk.json');
 const dataInfoSeed = require('./data/dataLabelManifests.json');
 
 const collection = 'tractinfo';
+const seedConfig = {
+  datainfo: {
+    active: true,
+    filepath: "../data/datainfo.json",
+    overwrite: true
+  },
+  tractdata: {
+    active: true,
+    filepath: "../data/tractdata.json",
+    overwrite: true
+  },
+  tractinfo: {
+    active: true,
+    filepath: "../data/tractinfo.json",
+    overwrite: true
+  },
+}
 
-const tractInfoWCityArray = collection === 'tractinfo' ? 
-  tractInfoSeed.map(tract => {
-    const tractObj = {...tract};
-    tractObj.cities = [];
-    cityCrossWalkSeed.forEach(tractWCity => 
-      tractWCity.GEOID === tractObj.GEOID ?
-        tractObj.cities.push(tractWCity.Cities) 
-      : null);
-      console.log(tractObj)
-    return tractObj;
-    }
-  ) : null;
-
-const content = 
-// ternary for each collection here
-  collection === 'tractinfo' ? 
-    tractInfoWCityArray
-  :collection === 'tractdata' ? 
-    tractDataSeed
-  :collection === 'datainfoseed' ? 
-    dataInfoSeed
-  : null; 
-
+// const tractInfoWCityArray = collection === 'tractinfo' ? 
+//   tractInfoSeed.map(tract => {
+//     const tractObj = {...tract};
+//     tractObj.cities = [];
+//     cityCrossWalkSeed.forEach(tractWCity => 
+//       tractWCity.GEOID === tractObj.GEOID ?
+//         tractObj.cities.push(tractWCity.Cities) 
+//       : null);
+//       console.log(tractObj)
+//     return tractObj;
+//     }
+//   ) : null;
 
 
 mongoose.connect(MONGODB_URI,
-  { useNewUrlParser: true,
+  {
+    useNewUrlParser: true,
     useUnifiedTopology: true
   }
-); 
+);
+
+
+Object.entries(seedConfig).forEach(([key, value]) => {
+  const seed = require(value.filepath);
+
+  key === 'tractinfo' ?
+    tractInfoSeed.map(tract => {
+      const tractObj = { ...tract };
+      tractObj.cities = [];
+      cityCrossWalkSeed.forEach(tractWCity =>
+        tractWCity.GEOID === tractObj.GEOID ?
+          tractObj.cities.push(tractWCity.Cities)
+          : null)
+
+      value.overwrite && db[key] ?
+        // then remove and insert
+        db[key]
+          .remove()
+          .then(() =>
+            db[key].insertMany(seed))
+          .then(data => {
+            console.log(data.length + " records inserted!");
+            process.exit(0);
+          })
+          .catch(err => {
+            console.error(err);
+            process.exit(1);
+          })
+        : // or else if not true then just insert without removing
+        db[key].insertMany(seed)
+      .then(data => {
+        console.log(data.length + " records inserted!");
+        process.exit(0);
+      })
+      .catch(err => {
+        console.error(err);
+        process.exit(1);
+      }) 
+      
+    
+
+    })
+
+
+
+
+
+// const content = 
+// // ternary for each collection here
+//   collection === 'tractinfo' ? 
+//     tractInfoWCityArray
+//   :collection === 'tractdata' ? 
+//     tractDataSeed
+//   :collection === 'datainfoseed' ? 
+//     dataInfoSeed
+//   : null; 
+
+
+
+
 
 // Use insertMany 
 
-db[collection]
-  .remove()
-  .then(() => 
-  db[collection].insertMany(content))
-    .then(data => {
-      console.log(data.length + " records inserted!");
-      process.exit(0);
-    })
-    .catch(err => {
-      console.error(err);
-      process.exit(1);
-    });
+// db[collection]
+//   .remove()
+//   .then(() => 
+//   db[collection].insertMany(content))
+//     .then(data => {
+//       console.log(data.length + " records inserted!");
+//       process.exit(0);
+//     })
+//     .catch(err => {
+//       console.error(err);
+//       process.exit(1);
+//     });
 
