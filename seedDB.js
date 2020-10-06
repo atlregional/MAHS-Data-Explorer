60c05b81ecf4cd4865365750869cc1ca44e5774e
aagrosse <aagrock81@yahoo.com>
2020-10-06 02:20:43 -0400

    edit on seed function

--- seedDB.js
+++ seedDB.js
@@ -3,12 +3,26 @@ const mongoose = require("mongoose");
 const db = require("./models");
 const MONGODB_URI = process.env.MONGODB_URI;
 
-const tractDataSeed = require('./data/tractData.json');
+// const tractDataSeed = require('./data/tractData.json');
 const tractInfoSeed = require('./data/tractInfo.json');
 const cityCrossWalkSeed = require('./data/tractToCityCrosswalk.json');
-const dataInfoSeed = require('./data/dataLabelManifests.json');
+// const dataInfoSeed = require('./data/dataLabelManifests.json');
 
-const collection = 'tractinfo';
+// const collection = 'tractinfo';
+const infoSeed = () => {
+tractInfoSeed.map(tract => {
+  const tractObj = { ...tract };
+  tractObj.cities = [];
+
+  cityCrossWalkSeed.forEach(tractWCity =>
+    tractWCity.GEOID === tractObj.GEOID ?
+      tractObj.cities.push(tractWCity.Cities)
+      : null)
+      return tractObj;
+})
+
+}
+
 const seedConfig = {
   datainfo: {
     active: true,
@@ -22,7 +36,7 @@ const seedConfig = {
   },
   tractinfo: {
     active: true,
-    filepath: "../data/tractinfo.json",
+    filepath: infoSeed(),
     overwrite: true
   },
 }
@@ -52,43 +66,51 @@ mongoose.connect(MONGODB_URI,
 Object.entries(seedConfig).forEach(([key, value]) => {
   const seed = require(value.filepath);
 
-  key === 'tractinfo' ?
-    tractInfoSeed.map(tract => {
-      const tractObj = { ...tract };
-      tractObj.cities = [];
-      cityCrossWalkSeed.forEach(tractWCity =>
-        tractWCity.GEOID === tractObj.GEOID ?
-          tractObj.cities.push(tractWCity.Cities)
-          : null)
+    value.overwrite && db[key] ?
+      // then remove and insert
+      db[key]
+        .remove()
+        .then(() =>
+          db[key].insertMany(seed))
+        .then(data => {
+          console.log(data.length + " records inserted!");
+          process.exit(0);
+        })
+        .catch(err => {
+          console.error(err);
+          process.exit(1);
+        })
+      : // or else if not true then just insert without removing
+      db[key].insertMany(seed)
+        .then(data => {
+          console.log(data.length + " records inserted!");
+          process.exit(0);
+        })
+        .catch(err => {
+          console.error(err);
+          process.exit(1);
+        })
 
-      value.overwrite && db[key] ?
-        // then remove and insert
-        db[key]
-          .remove()
-          .then(() =>
-            db[key].insertMany(seed))
-          .then(data => {
-            console.log(data.length + " records inserted!");
-            process.exit(0);
-          })
-          .catch(err => {
-            console.error(err);
-            process.exit(1);
-          })
-        : // or else if not true then just insert without removing
-        db[key].insertMany(seed)
-      .then(data => {
-        console.log(data.length + " records inserted!");
-        process.exit(0);
-      })
-      .catch(err => {
-        console.error(err);
-        process.exit(1);
-      }) 
-      
-    
 
-    })
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
