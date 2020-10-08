ffcacb3c9cf796d8b5730a31c449f96077bd049a
John Sasser <cw.john.sasser@gmail.com>
2020-10-08 14:24:22 -0400

    working subarea, still move the selectedSubarea state up to HomePage

--- client/src/components/SubAreaSelector/index.js
+++ client/src/components/SubAreaSelector/index.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import './style.css';
 
 const SubAreaSelector = (props) => {
-<<<<<<< HEAD
 
   const [selectedSubareas, setSelectedSubareas] = useState([])
 
@@ -35,11 +34,6 @@ const SubAreaSelector = (props) => {
 
   useEffect(() => subareas, []);
   return subareas ? (
-=======
-  const subareas = props.subareaOptions;
-  const colormap = props.colormap
-  return (
->>>>>>> 528ab7c5880903e73a84382784748e9658b1d6d3
     <div id="subarea-selector-container">
       {subareas.map((subarea) => (
         <div
