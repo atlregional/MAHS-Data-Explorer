0c9ae557e83c356b8c007dd7db5d13547727e74c
Erik Woodworth <ewoodworth@atlantaregional.org>
2020-10-09 14:08:19 -0400

    removed proxy

--- client/src/setupProxy.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
-module.exports = app => app.use(createProxyMiddleware("/*", 
-process.env.NODE_ENV !== 'production' ?
-{ target: "http://localhost:3001" }
-: { target: `${__dirname}:${process.env.PORT}/`}));
