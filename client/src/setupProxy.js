1c9b1ebbae03d6fe792141840838d004e182f654
Erik Woodworth <ewoodworth@atlantaregional.org>
2020-10-09 13:36:02 -0400

    extracted createdProxyMiddleware from http-proxy-middleware to setup middleware in src

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,3 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function(app) {
-  app.use(
-    proxy(["/*"], { target: "http://localhost:3001" })
-  );
-};
+module.exports = app => app.use(createProxyMiddleware("/*", { target: "http://localhost:3001" }));
