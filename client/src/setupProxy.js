4c1d90e9a57b1de4aed6a43d873fe3404bb4add1
Erik Woodworth <ewoodworth@atlantaregional.org>
2020-10-09 13:53:48 -0400

    added production condition to setupProxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = app => app.use(createProxyMiddleware("/*", { target: "http://localhost:3001" }));
+module.exports = app => app.use(createProxyMiddleware("/*", 
+process.env.NODE_ENV !== 'production' ?
+{ target: "http://localhost:3001" }
+: { target: `${__dirname}:${process.env.PORT}/`}));
