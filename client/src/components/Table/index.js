b67fca146024484f01a3393c9093c0b1d8a5bc55
aagrosse <aagrock81@yahoo.com>
2020-10-22 10:00:41 -0400

    added table aggregator

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StickyTable, Row, Cell } from 'react-sticky-table';
 import utils from '../../utils';
 
 
-const Table = () => {
+const Table = props => {
   // Bring in data and map to table with indicator name as row headers and subarea name as column headers
-  const indicators = [
+  const tractInfo = props.tractInfo;
+
+  const [ data, setData ] = useState();
+  const indicatorInfo = [
     {
       name: "Average Number of HHs by Tract, 2017",
       type: "average",
@@ -44,7 +47,61 @@ const Table = () => {
     }
   ];
 
-  return <>{/* put a fancy Table component in here. */}Table</>;
+  const handleAggregation = () => {
+    const array = [];
+    const aggregatedData = utils.aggregate(tractInfo, indicatorInfo, 'Subarea');
+
+    Object.entries(aggregatedData).forEach(([key, value]) =>
+      array.push({
+        name: key,
+        Subarea: parseInt(key.replace('Subarea ', '')),
+        [indicatorInfo.name] : value
+      })
+    );
+
+    array.sort((a,b) => a.Subarea < b.Subarea ? -1 : 1)
+    setData(array)
+
+  }
+    // : null;
+  console.log(data);
+
+  useEffect(handleAggregation, [])
+
+  return (
+    <div>
+      <div style={{width: '100%', height: '400px'}}>
+        <StickyTable
+        stickHeaderCount={1}>
+          <Row>
+          {data.map(field => 
+          
+            <Cell
+              style={{
+              
+                width: '100px',
+                textAlign: 'center', 
+                verticalAlign: 'middle', 
+                backgroundColor: '#d5bdbd',
+                padding: '4px'
+              }}
+              key={'column-' + field} 
+              
+              value={'field'} >
+              </Cell>
+           )}
+            
+
+            
+          </Row>
+          <Row>
+            <Cell>Cell 1</Cell>
+            <Cell>Cell 2</Cell>
+          </Row>
+        </StickyTable>
+      </div>
+    </div>
+  );
 };
 
 export default Table;
