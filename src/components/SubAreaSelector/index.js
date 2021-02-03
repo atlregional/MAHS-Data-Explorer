3a289bfefdf598679fc0d280ae63623806e90911
John Sasser <cw.john.sasser@gmail.com>
2021-02-03 11:25:49 -0500

    moved the sumarket obj

--- src/components/SubAreaSelector/index.js
+++ src/components/SubAreaSelector/index.js
@@ -1,114 +1,109 @@
-import React, { useEffect, useState } from 'react';
-import { Popup } from 'semantic-ui-react';
-import { submarkets } from '../../index.js';
-import './style.css';
-// import { imageOverlay } from 'leaflet';
+import React, { useEffect, useState } from "react";
+import { Popup } from "semantic-ui-react";
+import { submarkets } from "../../utils/submarkets";
+import "./style.css";
 
-const SubAreaSelector = props => {
-  const [buttonSize, setButtonSize] = useState(75);
-  const windowWidth = window.innerWidth;
-  const subareas = props.subareaOptions;
-  const numberOfSubareas = subareas.length;
-  const scaler = numberOfSubareas < 6 ? 1.25 : windowWidth > 1100 ? 2.5 : 2;
-  const highlightedSubarea = props.highlightedSubarea;
-  const colormap = props.colormap;
-  const buttonMargin = windowWidth > 800 ? '3.75px' : '1px';
+const SubAreaSelector = (props) => {
+	const [buttonSize, setButtonSize] = useState(75);
+	const windowWidth = window.innerWidth;
+	const subareas = props.subareaOptions;
+	const numberOfSubareas = subareas.length;
+	const scaler = numberOfSubareas < 6 ? 1.25 : windowWidth > 1100 ? 2.5 : 2;
+	const highlightedSubarea = props.highlightedSubarea;
+	const colormap = props.colormap;
+	const buttonMargin = windowWidth > 800 ? "3.75px" : "1px";
 
-  const setHighlightedSubarea = number => props.setHighlightedSubarea(number);
+	const setHighlightedSubarea = (number) => props.setHighlightedSubarea(number);
 
-  const handleButtonSize = () =>
-    setButtonSize(
-      windowWidth > 800 ? (windowWidth * 0.1) / scaler : windowWidth / 6.5 - 15
-    );
-  console.log('window width: ', windowWidth / 2 - 15);
+	const handleButtonSize = () =>
+		setButtonSize(
+			windowWidth > 800 ? (windowWidth * 0.1) / scaler : windowWidth / 6.5 - 15
+		);
+	console.log("window width: ", windowWidth / 2 - 15);
 
-  useEffect(handleButtonSize, [subareas]);
+	useEffect(handleButtonSize, [subareas]);
 
-  return subareas ? (
-    <div id="subarea-selector-container">
-      {subareas.map(
-        subarea => (
-          // console.log(submarkets[subarea].link),
-          (
-            <Popup
-              hoverable
-              position="right center"
-              key={submarkets[subarea].name}
-              header={
-                <div
-                  className="popover-header"
-                  style={{
-                    color: `${colormap[subarea - 1]}`,
-                    backgroundColor: '#FEFEFE',
-                    marginBottom: '5px',
-                  }}
-                >
-                  {' '}
-                  {submarkets[subarea].name}
-                </div>
-              }
-              content={
-                <>
-                  <div style={{ margin: '10px 0 5px 0' }}>
-                    {submarkets[subarea].description}
-                  </div>
-                  <a href={`${submarkets[subarea].link}`} target="blank">
-                    More Info
-                  </a>
-                </>
-              }
-              trigger={
-                <div
-                  key={`subarea-selector-button-${subarea}-${highlightedSubarea}`}
-                  className="subarea-selector-button"
-                  style={{
-                    // height: `${buttonSize}px`,
-                    // width: `${buttonSize * 1.5}px`,
-                    // margin: buttonMargin,
-                    // lineHeight: `${buttonSize - buttonSize * 0.05}px`,
-                    backgroundColor: `${colormap[subarea - 1]}`,
-                    opacity:
-                      highlightedSubarea === subarea
-                        ? '1'
-                        : highlightedSubarea
-                        ? props.clickedSubarea
-                          ? '.3'
-                          : '.6'
-                        : '1',
-                    borderColor:
-                      highlightedSubarea === subarea
-                        ? 'black'
-                        : `${colormap[subarea - 1]}`,
-                    borderWidth: '3px',
-                    borderStyle: 'solid',
-                  }}
-                  onMouseEnter={() => {
-                    setHighlightedSubarea(
-                      props.clickedSubarea ? props.clickedSubarea : subarea
-                    );
-                  }}
-                  onMouseLeave={() => {
-                    setHighlightedSubarea(props.clickedSubarea);
-                  }}
-                  onClick={() => {
-                    props.setClickedSubarea(
-                      props.clickedSubarea
-                        ? subarea === props.clickedSubarea
-                          ? null
-                          : props.clickedSubarea
-                        : subarea
-                    );
-                  }}
-                >
-                  {subarea}
-                </div>
-              }
-            />
-          )
-        )
-      )}
-    </div>
-  ) : null;
+	return subareas ? (
+		<div id="subarea-selector-container">
+			{subareas.map((subarea) => (
+				// console.log(submarkets[subarea].link),
+				<Popup
+					hoverable
+					position="right center"
+					key={submarkets[subarea].name}
+					header={
+						<div
+							className="popover-header"
+							style={{
+								color: `${colormap[subarea - 1]}`,
+								backgroundColor: "#FEFEFE",
+								marginBottom: "5px",
+							}}
+						>
+							{" "}
+							{submarkets[subarea].name}
+						</div>
+					}
+					content={
+						<>
+							<div style={{ margin: "10px 0 5px 0" }}>
+								{submarkets[subarea].description}
+							</div>
+							<a href={`${submarkets[subarea].link}`} target="blank">
+								More Info
+							</a>
+						</>
+					}
+					trigger={
+						<div
+							key={`subarea-selector-button-${subarea}-${highlightedSubarea}`}
+							className="subarea-selector-button"
+							style={{
+								// height: `${buttonSize}px`,
+								// width: `${buttonSize * 1.5}px`,
+								// margin: buttonMargin,
+								// lineHeight: `${buttonSize - buttonSize * 0.05}px`,
+								backgroundColor: `${colormap[subarea - 1]}`,
+								opacity:
+									highlightedSubarea === subarea
+										? "1"
+										: highlightedSubarea
+										? props.clickedSubarea
+											? ".3"
+											: ".6"
+										: "1",
+								borderColor:
+									highlightedSubarea === subarea
+										? "black"
+										: `${colormap[subarea - 1]}`,
+								borderWidth: "3px",
+								borderStyle: "solid",
+							}}
+							onMouseEnter={() => {
+								setHighlightedSubarea(
+									props.clickedSubarea ? props.clickedSubarea : subarea
+								);
+							}}
+							onMouseLeave={() => {
+								setHighlightedSubarea(props.clickedSubarea);
+							}}
+							onClick={() => {
+								props.setClickedSubarea(
+									props.clickedSubarea
+										? subarea === props.clickedSubarea
+											? null
+											: props.clickedSubarea
+										: subarea
+								);
+							}}
+						>
+							{subarea}
+						</div>
+					}
+				/>
+			))}
+		</div>
+	) : null;
 };
 
 export default SubAreaSelector;
--- src/index.js
+++ src/index.js
@@ -4,74 +4,13 @@ import App from "./App";
 import "normalize.css";
 import "./index.css";
 
-const submarkets = {
-  1: {
-    name: "SUBMARKET 1",
-    description: "Higher-priced core neighborhoods.",
-    link: "https://metroatlhousing.org/submarkets/submarket-1/"
-  },
-  2: {
-    name: "SUBMARKET 2",
-    description:
-      "Higher-priced near core and employment corridor neighborhoods.",
-    link: "https://metroatlhousing.org/submarkets/submarket-2/"
-  },
-  3: {
-    name: "SUBMARKET 3",
-    description:
-      "Rapidly changing core neighborhoods experiencing the greatest increase in housing costs regionally.",
-    link: "https://metroatlhousing.org/submarkets/submarket-3/"
-  },
-  4: {
-    name: "SUBMARKET 4",
-    description:
-      "Lower-priced core neighborhoods vulnerable to increasing housing costs.",
-    link: "https://metroatlhousing.org/submarkets/submarket-4/"
-  },
-  5: {
-    name: "SUBMARKET 5",
-    description:
-      "Suburban neighborhoods along employment corridors with moderate-to-higher-priced mix of single family and multifamily housing.",
-    link: "https://metroatlhousing.org/submarkets/submarket-5/"
-  },
-  6: {
-    name: "SUBMARKET 6",
-    description:
-      "Suburban neighborhoods with moderate-to-higher-priced housing.",
-    link: "https://metroatlhousing.org/submarkets/submarket-6/"
-  },
-  7: {
-    name: "SUBMARKET 7",
-    description:
-      "Suburban neighborhoods with lower-to-moderate-priced housing, biggest increase in renters.",
-    link: "https://metroatlhousing.org/submarkets/submarket-7/"
-  },
-  8: {
-    name: "SUBMARKET 8",
-    description:
-      "Suburban neighborhoods with lowest-priced single-family homes, mix of renters and owners.",
-    link: "https://metroatlhousing.org/submarkets/submarket-8/"
-  },
-  9: {
-    name: "SUBMARKET 9",
-    description: "Lower-priced rural areas.",
-    link: "https://metroatlhousing.org/submarkets/submarket-9/"
-  },
-  10: {
-    name: "SUBMARKET 10",
-    description: "Higher-priced rural areas.",
-    link: "https://metroatlhousing.org/submarkets/submarket-10/"
-  }
-};
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-export { submarkets };
