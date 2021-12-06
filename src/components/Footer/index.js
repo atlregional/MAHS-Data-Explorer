df1c0b3cf9ccf237a3feee4747d32240b925c110
Erik Woodworth <ewoodworth@atlantaregional.org>
2021-12-06 11:03:26 -0600

    added ARC logo

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,24 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 const Footer = () => {
   return (
-    <div className="footer-parent-div">
+    <div className='footer-parent-div'>
       <p>created by</p>
-      <img
-        src="https://neighborhoodnexus.org/wp-content/uploads/2016/04/neighborhood-nexus-logo.png"
-        alt="ARC-logo"
-      />
+      <div id='footer-logos'>
+        <img
+          src='https://i2.wp.com/neighborhoodnexus.org/wp-content/uploads/2021/12/ARC_logo_PMS7549_PMS425-e1638809577931.png'
+          alt='ARC-logo'
+        />
+        <img
+          src='https://neighborhoodnexus.org/wp-content/uploads/2016/04/neighborhood-nexus-logo.png'
+          alt='NN-logo'
+        />
+      </div>
+
     </div>
   );
 };
 export default Footer;
+
+
