605345091f7472701672efe8fb525444212ae876
Erik Woodworth <ewoodworth@atlantaregional.org>
2022-01-25 13:42:32 -0600

    Fixed broken logo urls

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,11 +7,11 @@ const Footer = () => {
       <p>created by</p>
       <div id='footer-logos'>
         <img
-          src='https://i2.wp.com/neighborhoodnexus.org/wp-content/uploads/2021/12/ARC_logo_PMS7549_PMS425-e1638809577931.png'
+          src='https://atlantaregional.org/wp-content/uploads/arc-logo-webinar.png'
           alt='ARC-logo'
         />
         <img
-          src='https://neighborhoodnexus.org/wp-content/uploads/2016/04/neighborhood-nexus-logo.png'
+          src='https://neighborhoodnexus.org/wp-content/uploads/2022/01/neighborhood-nexus-logo.png'
           alt='NN-logo'
         />
       </div>
